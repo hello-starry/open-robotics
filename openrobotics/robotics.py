a2c0300eaa05ad4cc4531768cf0023ce8abfdb09
JinminCHEN <chenjm1109@foxmail.com>
2021-09-30 15:17:49 +0800

    增加stewart运动学算法

--- openrobotics/robotics.py
+++ openrobotics/robotics.py
@@ -1,17 +1,24 @@
+from numpy import ndarray
 from openrobotics.datastruct import *
+
 
 class Robot(object):
     def __init__(self) -> None:
         super().__init__()
 
+
 class Scara(Robot):
     def __init__(self) -> None:
         super().__init__()
+
 
 class Delta(Robot):
     def __init__(self) -> None:
         super().__init__()
 
+
 class R6(Robot):
     def __init__(self) -> None:
         super().__init__()
+
+
