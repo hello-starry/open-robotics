477cec71e7035e8f90e3fd53d0528b8634e829fa
JinminCHEN <chenjm1109@foxmail.com>
2021-09-30 12:00:22 +0800

    建立了模块结构

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="open-robotics",
+    version="1.0",
+    author="JinminCHEN",
+    author_email="chenjm1109@foxmail.com",
+    description="Open Robotics 开源机器人学库",
+    url="https://chenjm1109.github.io/open-robotics/", 
+    packages=find_packages()
+)
